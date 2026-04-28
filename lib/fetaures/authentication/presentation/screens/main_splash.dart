81e1e1eaa5eb29993713d2504327465210455ee6
ameena m <ameena.techzera@gmail.com>
2026-04-28 11:40:13 +0530

    pushon28042026morning

--- lib/fetaures/authentication/presentation/screens/main_splash.dart
+++ lib/fetaures/authentication/presentation/screens/main_splash.dart
@@ -1,3 +1,96 @@
+// import 'package:flutter/material.dart';
+// import 'package:studentmanagement/core/appdata/appdata.dart';
+// import 'package:studentmanagement/fetaures/authentication/presentation/screens/second_splash.dart';
+// import 'package:studentmanagement/fetaures/home_screen/presentation/screens/main_screen.dart';
+// import 'package:studentmanagement/services/shared_preference_helper.dart';
+
+// class MainSplashScreen extends StatefulWidget {
+//   const MainSplashScreen({super.key});
+
+//   @override
+//   State<MainSplashScreen> createState() => _MainSplashScreenState();
+// }
+
+// class _MainSplashScreenState extends State<MainSplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _checkLogin();
+//   }
+
+//   Future<void> _checkLogin() async {
+//     final sharedPrefHelper = SharedPreferenceHelper();
+
+//     final loginResponse = await sharedPrefHelper.getLoginResponse();
+//     AppData.admissionNo = loginResponse?.student!.admno.toString();
+//     AppData.studentName = loginResponse?.student!.name.toString();
+//     ;
+//     AppData.studentStdId = loginResponse?.student!.currentStudentStandardId
+//         .toString();
+//     ;
+//     AppData.studentDivId = loginResponse?.student!.currentStudentDivisionId
+//         .toString();
+//     AppData.accYear = loginResponse?.student!.accYear.toString();
+
+//     await Future.delayed(const Duration(seconds: 2));
+
+//     if (loginResponse != null) {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(
+//           builder: (_) => MainScreen(loginResponse: loginResponse),
+//         ),
+//       );
+//     } else {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (_) => SecondSplashScreen()),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: SafeArea(
+//         child: Center(
+//           child: Column(
+//             children: [
+//               const Spacer(flex: 2),
+
+//               /// TOP LOGO
+//               Image.asset('assets/images/fsp_logo.png', height: 250),
+
+//               const SizedBox(height: 20),
+
+//               /// SCHOOL NAME
+//               const Text(
+//                 'First Step Preschool',
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                   fontWeight: FontWeight.w600,
+//                   color: Colors.black,
+//                 ),
+//               ),
+
+//               const Spacer(flex: 3),
+
+//               /// BOTTOM IMAGE / BRAND
+//               Image.asset(
+//                 'assets/images/cristal_horizontal.png',
+//                 height: 200,
+//                 fit: BoxFit.contain,
+//               ),
+
+//               const SizedBox(height: 12),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:studentmanagement/core/appdata/appdata.dart';
 import 'package:studentmanagement/fetaures/authentication/presentation/screens/second_splash.dart';
@@ -12,10 +105,25 @@ class MainSplashScreen extends StatefulWidget {
 }
 
 class _MainSplashScreenState extends State<MainSplashScreen> {
+  String? branchName;
+  String? logo;
   @override
   void initState() {
     super.initState();
+    _loadBranchData();
     _checkLogin();
+  }
+
+  Future<void> _loadBranchData() async {
+    final pref = SharedPreferenceHelper();
+    final branch = await pref.getBranchData();
+
+    if (branch != null) {
+      setState(() {
+        branchName = branch['branchName'];
+        logo = branch['logo']; // 👉 only one logo
+      });
+    }
   }
 
   Future<void> _checkLogin() async {
@@ -49,6 +157,24 @@ class _MainSplashScreenState extends State<MainSplashScreen> {
     }
   }
 
+  /// 🔥 COMMON LOGO (API or fallback)
+  Widget buildLogo(double height) {
+    return logo != null && logo!.isNotEmpty
+        ? Image.network(
+            logo!,
+            height: height,
+            fit: BoxFit.contain,
+            errorBuilder: (_, __, ___) => Image.asset(
+              'assets/images/cristal_horizontal.png', // fallback
+              height: height,
+            ),
+          )
+        : Image.asset(
+            'assets/images/cristal_horizontal.png', // fallback
+            height: height,
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,15 +184,15 @@ class _MainSplashScreenState extends State<MainSplashScreen> {
           child: Column(
             children: [
               const Spacer(flex: 2),
+              buildLogo(250),
 
               /// TOP LOGO
-              Image.asset('assets/images/fsp_logo.png', height: 250),
-
+              //  Image.asset('assets/images/fsp_logo.png', height: 250),
               const SizedBox(height: 20),
 
               /// SCHOOL NAME
-              const Text(
-                'First Step Preschool',
+              Text(
+                branchName ?? 'School Name',
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.w600,
@@ -77,11 +203,7 @@ class _MainSplashScreenState extends State<MainSplashScreen> {
               const Spacer(flex: 3),
 
               /// BOTTOM IMAGE / BRAND
-              Image.asset(
-                'assets/images/cristal_horizontal.png',
-                height: 200,
-                fit: BoxFit.contain,
-              ),
+              buildLogo(150),
 
               const SizedBox(height: 12),
             ],
